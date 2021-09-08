9f36ea4be9b88551a07c5b9dcfba5e0fbde08479
Favio Rivadeneyra <faviofro86@gmail.com>
2021-09-08 14:43:37 -0500

    Corrección en impresión de valores

--- includes/valores.php
+++ includes/valores.php
@@ -1,12 +1,24 @@
 <?php
 
 include_once('conexion.php');
-include_once('usuarios.php');
+include_once('usuario.php');
 $dni=$_GET['dni'];
 
 $user = new Usuario;
 
 $grasa = $user->grasa($dni);
 echo json_encode($grasa);
+
+$imc = $user->imc($dni);
+echo json_encode($imc);
+
+$peso = $user->peso($dni);
+echo json_encode($peso);
+
+$cintura = $user->cintura($dni);
+echo json_encode($cintura);
+
+$brazo = $user->brazo($dni);
+echo json_encode($brazo);
 
 ?>
