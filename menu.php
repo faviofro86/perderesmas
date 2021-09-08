de6860fe50870350bd6579cc418a8d69b7cbcaf9
Favio Rivadeneyra <faviofro86@gmail.com>
2021-09-07 21:52:07 -0500

    Correcciones menu

--- menu.php
+++ menu.php
@@ -2,12 +2,12 @@
             <img id="menuBtnCerrar" class="menuBtnCerrar" src="https://pipartners.cl/wp-content/uploads/2021/07/boton-cerrar.png" alt="cerrar">
             <nav class="headerNav">
                 <ul class="menu d-flex align-items-center">
-                    <li class="menu__li"><a class="menu__a inicio" href="home" rel="noopener noreferrer">Inicio</a></li>
-                    <li class="menu__li bordesLaterales"><a class="menu__a progreso" href="progreso" rel="noopener noreferrer">Progreso</a></li>
-                    <li class="menu__li"><a class="menu__a premios" href="premios" rel="noopener noreferrer">Premios</a></li>
-                    <li class="menu__li bordesLaterales"><a class="menu__a menuTycColor" href="terminos_y_condiciones" rel="noopener noreferrer">T y C</a></li>
-                    <li class="menu__li perfil"><a class="menu__a" href="perfil" rel="noopener noreferrer">Perfil</a></li>
-                    <li class="menu__li"><a class="menu__a" href="logout" rel="noopener noreferrer">Salir</a></li>
+                    <li class="menu__li"><a class="menu__a inicio" href="home.php" rel="noopener noreferrer">Inicio</a></li>
+                    <li class="menu__li bordesLaterales"><a class="menu__a progreso" href="progreso.php" rel="noopener noreferrer">Progreso</a></li>
+                    <li class="menu__li"><a class="menu__a premios" href="premios.php" rel="noopener noreferrer">Premios</a></li>
+                    <li class="menu__li bordesLaterales"><a class="menu__a menuTycColor" href="terminos_y_condiciones.php" rel="noopener noreferrer">T y C</a></li>
+                    <li class="menu__li perfil"><a class="menu__a" href="perfil.php" rel="noopener noreferrer">Perfil</a></li>
+                    <li class="menu__li"><a class="menu__a" href="logout.php" rel="noopener noreferrer">Salir</a></li>
                 </ul>
             </nav>
             <div class="socialMedia d-flex align-items-center">
